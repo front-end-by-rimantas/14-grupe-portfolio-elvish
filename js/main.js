20f480d900f5389c683a62a74703aa89f24f98e6
minde8888 <58484005+minde8888@users.noreply.github.com>
2020-04-16 12:51:05 +0300

    Update main.js
    count numbers

--- js/main.js
+++ js/main.js
@@ -3,3 +3,31 @@ import Achievements from './Achievements.js';
 
 //create four achievements blocks
 new Achievements('#achievements > .row');
+
+/*const counters = document.querySelectorAll('.counter');
+const speed = 200; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('achievements');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = count + inc;
+			// Call function every ms
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});*/
