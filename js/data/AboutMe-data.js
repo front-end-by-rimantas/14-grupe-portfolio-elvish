b877250e5f16de8804de160a4cb850105de8f4e7
Viking <37813004+Vikinges@users.noreply.github.com>
2020-04-19 13:08:50 +0300

    fixBlood&nbsp;

--- js/data/AboutMe-data.js
+++ js/data/AboutMe-data.js
@@ -7,7 +7,7 @@ const aboutData = [
         titleShort:'It is a long established fact that a reader will be distracted \
                     by the readable content of a page when looking at its layout.',
         icon: './img/signature.PNG',
-        hello: 'Hello!,I\'M Elvish Walters,',
+        hello: 'Hello! <b>&nbsp;I\'M Elvish Walters,</b>',
         name1: 'UI/UX Designer Photographer Devloper',
         // name2: 'Phatographer',
         // name3: 'Developer',
