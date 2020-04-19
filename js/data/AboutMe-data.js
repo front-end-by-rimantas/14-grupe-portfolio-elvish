89d63bc7d71bd108a5923d590067a76f8328d255
Viking <37813004+Vikinges@users.noreply.github.com>
2020-04-19 16:34:42 +0300

    Gixbug

--- js/data/AboutMe-data.js
+++ js/data/AboutMe-data.js
@@ -1,16 +1,12 @@
 "use strict";
 
 const aboutData = [
-    {
-        // aboutMe:'About Me',
-        // icon:'*',
+    {    
         titleShort:'It is a long established fact that a reader will be distracted \
-                    by the readable content of a page when looking at its layout.',
+               by the readable content of a page when looking at its layout.',
         icon: './img/signature.PNG',
         hello: 'Hello! <b>&nbsp;I\'M Elvish Walters,</b>',
-        name1: 'UI/UX Designer Photographer Devloper',
-        // name2: 'Phatographer',
-        // name3: 'Developer',
+        name1: 'UI/UX Designer Photographer Developer',      
         titleLong: ' It is a long established fact that a reader will be distracted \
         by the readable content of a page when looking at its layout.The point of\
         using Lorem Ipsum is that it has a more-or-less normal distribution of letters,\
