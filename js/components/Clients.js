b357c8b0ab84a7da32305bcf0fab4825902379c5
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-04-24 21:39:00 +0300

    testimonials static update

---
+++ js/components/Clients.js
@@ -0,0 +1,81 @@
+"use strict";
+import data from '../data/clients-data.js';
+
+class Clients {
+    constructor(target) {
+        this.target = target;
+        this.middleIndex = Math.floor(data.length / 2);
+        this.init();
+    }
+
+    init() {
+        if (this.isValid()) {
+            this.render();
+        }
+    }
+
+    isValid() {
+        const DOM = document.querySelector(this.target);
+        if (!DOM) {
+            throw 'ERROR: target selector is incorrect...';
+        }
+        this.DOM = DOM;
+
+        if (!Array.isArray(data) || data.length === 0 ) {
+            throw 'ERROR: data must be a non-empty array...';
+        }
+
+        return true;
+    }
+
+    render() {
+        const HTML = `
+            <div class="testimonials-list">
+                <div class="testimonial-block"></div>
+            </div>
+                <div class="controls"></div>            
+            `;
+        this.DOM.innerHTML = HTML;
+        this.DOMtestimonials = this.DOM.querySelector('.testimonials-list');
+        this.DOMcontrols = this.DOM.querySelector('.controls');        
+
+        this.renderTestimonials();
+        this.renderControls();
+    }
+
+    renderTestimonials() {        
+        let HTML = '';
+        for(let i=0; i<data.length; i++){
+            const block = data[i];
+            HTML += `
+                <div class="testimonial-block">
+                    <i class="fa fa-user-circle-o"></i>
+                    <div class="client">${block.client}</div>
+                    <div class="company">${block.company}</div>
+                    <div class="testimonial">"${block.testimonial}"</div>
+                </div>`;
+        }
+        this.DOMtestimonials.innerHTML = HTML;
+
+        this.DOMcurrentClient = document.querySelector('.testimonials-list');
+        this.DOMcurrentClient.style.marginLeft = '-100%';
+    }
+
+    renderControls() {
+        let HTML ='';
+        
+        for(let t=0; t<data.length; t++) {
+            
+            HTML += `<div class="control-${t+1}">
+                        <span></span>
+                    </div>`;
+        }
+                
+        //control-${middleIndex+1}.classList.add("active");
+
+        this.DOMcontrols.innerHTML = HTML;
+        this.DOMactiveControl = document.querySelector('.control-'+ (this.middleIndex+1));        
+        this.DOMactiveControl.classList.add("active");
+    }
+}
+export default Clients;
---
+++ js/data/clients-data.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const data = [
+    {
+        client: "Kirk McFall",
+        company: "Google",
+        testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat porro enim eligendi. Eligendi accusantium, magnam harum, minus corporis veritatis animi necessitatibus numquam doloremque ex ad, aperiam commodi quam?"
+    },
+    {
+        client: "Edward Evans",
+        company: "Google",
+        testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat porro enim eligendi. Eligendi accusantium, magnam harum, minus corporis veritatis animi necessitatibus numquam doloremque ex ad, aperiam commodi quam?"
+    },
+    {
+        client: "Antonio Hernandez",
+        company: "Google",
+        testimonial: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat porro enim eligendi. Eligendi accusantium, magnam harum, minus corporis veritatis animi necessitatibus numquam doloremque ex ad, aperiam commodi quam?"
+    }
+];
+
+export default data;
