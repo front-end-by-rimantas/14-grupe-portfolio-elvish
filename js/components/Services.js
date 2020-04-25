49aa590476d9a5e6e2d7d35012803d68d7c8d91f
Viking <37813004+Vikinges@users.noreply.github.com>
2020-04-25 11:41:35 +0300

    Add_services_data

--- js/components/Services.js
+++ js/components/Services.js
@@ -1,4 +1,7 @@
 "use strict";
+
+import data from '../data/Services.js';
+
 class Servicess{
     constructor(target)
 {
@@ -15,6 +18,23 @@ init() {
     this.DOM = DOM;
     this.render();
 }
+const DOM = document.querySelector(this.target);
+if(!DOM){
+    throw 'ERROR: OMG... go fix Achievements!!!';
+}
+let HTML = '';
+
+for(let i=0; i<data.length; i++){
+    const ach = data[i];
+    HTML += `
+        <div class="col-3 achievements">
+        <i class="fa fa-${ach.icon}"></i>
+        <div class="number" data-num=${ach.number}>${ach.number}</div>
+        <p>${ach.title}</p>
+        </div>`
+}
+DOM.innerHTML = HTML;
+this.DOMnumbers = DOM.querySelectorAll('.number');
 }
 
 
---
+++ js/data/Services.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const data = [
+    {
+        photo: './img/Services/1.png',
+        name: 'Graphic Design',
+        par: 'It is a long established fact that a reader will be distracted by the  readable content of a page when looking at its layout.',
+    },
+    {
+        photo: './img/Services/2.png',
+        name: 'Unlimited Color',
+        par: 'It is a long established fact that a reader will be distracted by the  readable content of a page when looking at its layout.',
+    }, {
+        photo: './img/Services/3.png',
+        name: 'Media Marketing',
+        par: 'It is a long established fact that a reader will be distracted by the  readable content of a page when looking at its layout.',
+    }, {
+        photo: './img/Services/4.png',
+        name: 'Unlimuted WIfi',
+        par: 'It is a long established fact that a reader will be distracted by the  readable content of a page when looking at its layout.',
+    }, {
+        photo: './img/Services/5.png',
+        name: 'Responsive Design',
+        par: 'It is a long established fact that a reader will be distracted by the  readable content of a page when looking at its layout.',
+    }, {
+        photo: './img/Services/6.png',
+        name: 'Easy to customize',
+        par: 'It is a long established fact that a reader will be distracted by the  readable content of a page when looking at its layout.',
+    }
+];
+export default data;
