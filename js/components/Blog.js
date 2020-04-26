2ee9115665afa6e5c2ca0a5eac41a22f36994df2
el Aiste <aiste.norei@gmail.com>
2020-04-26 12:01:41 +0300

    play btn css

--- js/components/Blog.js
+++ js/components/Blog.js
@@ -45,14 +45,17 @@ class Blog {
                         <a class="category">${post.category}</a>
                         <p class="date">${post.date} <a class="author">${post.author}</a></p>
                         <p class="description">${post.description}</p>
-                        <p class="read-more">${post.readmore}</p>
+                        <p class="read-more">${post.readmore}</p> 
                     </div>`;
         }
         if (post.media.video)    {
         HTML += `<div class="post col-4">
-                        <video class="postmedia video" width="325" height="220" controls>
-                        <source src="../img/blog/${post.media.video}" type="video/mp4">
+                        <div class="play-blue"></div><div class="play"></div> <video class="postmedia video" width="325" height="220">
+                            <source src="../img/blog/${post.media.video}" type="video/mp4">
+                          
+                        
                         </video>
+                        
                         <a class="title">${post.title}</a>
                         <a class="category">${post.category}</a>
                         <p class="date">${post.date} <a class="author">${post.author}</a></p>
