0b87bcd330634b38fcccce1590e6ccf6a15fe1a5
minde8888 <58484005+minde8888@users.noreply.github.com>
2020-04-26 16:30:54 +0300

    update

--- js/components/Hero.js
+++ js/components/Hero.js
@@ -3,8 +3,7 @@
 class Hero{
     constructor(target){
         this.target = target;
-        this.DOM = null;
-        
+                
         this.init();
        
     }
@@ -14,7 +13,50 @@ class Hero{
         if ( !DOM ) {
             throw 'ERROR: header target location was not found.';
         }
+    this.text()
+    }
+
+    text(){
+        const htext = document.querySelector('#text span');
+        const names = ['Elvish Walters.', 'A Developer.', 'Designer.'];
+
+        let counter = 0;
+        let sum = 0;
+
+        let interval = 3000; 
+        names.forEach((names, index) => {
+            sum += counter;
+            console.log(sum)
+        setTimeout(() => {
+           
+ 
+            let text = `<span>${names}</span>`;
+            htext.innerHTML = text;
+            
+        }, index * interval    )
+            
+           
+        })
     }
 }
 
 export default Hero;
+
+/*if (!htext.style.opacity) {
+    htext.style.opacity = 1;
+ }
+ if (htext.style.opacity > 0) {
+     htext.style.opacity -= 0.1;
+ } else {
+     clearInterval(fadeEffect);
+ }*/
+
+/* if (!htext.style.opacity) {
+    htext.style.opacity = 1;
+    interval
+    }
+    if (htext.style.opacity > 0) {
+        htext.style.opacity -= 0.2;
+    } else {
+        clearInterval(htext);
+    }*/
